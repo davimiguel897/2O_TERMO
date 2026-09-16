4f420b25a3d780661d37a736362ca5b26114cd42
davimiguel897 <davimiguelguerratnt@gmail.com>
2026-09-16 10:33:29 -0300

    Correcao2

--- 2O_TERMO/BCD/projeto/SMARTCOFFEE_FISICO-DAVI_GUERRA-copia.sql
+++ 2O_TERMO/BCD/projeto/SMARTCOFFEE_FISICO-DAVI_GUERRA-copia.sql
@@ -1,10 +1,22 @@
+CREATE DATABASE IF NOT EXISTS SMARTCOFFEE;
+USE SMARTCOFFEE;
+
+CREATE TABLE Pedidos (
+nome_produto VARCHAR(150) NOT NULL,
+status_pedido BOOLEAN NOT NULL,
+tipo_pedido varchar(60) not null,
+valor_total DECIMAL(10,2) NOT NULL,
+id_pedidos INT AUTO_INCREMENT PRIMARY KEY,
+data_hora datetime NOT NULL
+)
+
 CREATE TABLE Programa_fidelidade (
 id_programa_finalidade INT AUTO_INCREMENT PRIMARY KEY PRIMARY KEY,
 data_ultima_atualizacao DATE NOT NULL,
 saldo_pontos INT NOT NULL DEFAULT 0,
 historico_transferencias TEXT,
 desconto DECIMAL(5,2) NOT NULL,
-cliente INT NOT NULL,
+cliente INT NOT NULL
 )
 
 CREATE TABLE Categoria (
@@ -66,15 +78,6 @@ CPF CHAR(11) NOT NULL UNIQUE,
 data_admissao DATE NOT NULL
 )
 
-CREATE TABLE Pedidos (
-nome_produto VARCHAR(150) NOT NULL,
-status_pedido BOOLEAN NOT NULL,
-tipo_pedido varchar(60) not null,
-valor_total DECIMAL(10,2) NOT NULL,
-id_pedidos INT AUTO_INCREMENT PRIMARY KEY,
-data_hora datetime NOT NULL,
-)
-
 CREATE TABLE Pagamentos (
 id_pagamentos INT AUTO_INCREMENT PRIMARY KEY,
 status_pagamento INT NOT NULL,
@@ -85,10 +88,7 @@ debito BOOLEAN DEFAULT FALSE,
 credito BOOLEAN DEFAULT FALSE,
 vale_alimentacao BOOLEAN DEFAULT FALSE,
 data_hora_pagamento DATE NOT NULL,
-valor_pago DECIMAL(10,2) NOT NULL,
-PRIMARY KEY(id_pedidos,id_pagamentos),
-FOREIGN KEY(id_clientes) REFERENCES Clientes (id_clientes),
-FOREIGN KEY(id_funcionarios) REFERENCES Funcionarios (id_funcionarios)
+valor_pago DECIMAL(10,2) NOT NULL
 )
 
 CREATE TABLE Delivery (
@@ -129,7 +129,7 @@ id_produtos int,
 id_insumo int,
 id_consome int auto_increment primary key PRIMARY KEY,
 FOREIGN KEY(id_produtos) REFERENCES Produtos (id_produtos),
-FOREIGN KEY(id_insumo) REFERENCES Insumos (id_insumo)
+FOREIGN KEY(id_insumo) REFERENCES Estoque (id_insumo)
 )
 
 CREATE TABLE Gera (
@@ -146,5 +146,3 @@ id_entrega int auto_increment primary key PRIMARY KEY,
 FOREIGN KEY(id_delivery) REFERENCES Delivery (id_delivery),
 FOREIGN KEY(id_funcionarios) REFERENCES Funcionarios (id_funcionarios)
 )
-
-ALTER TABLE Programa_fidelidade ADD FOREIGN KEY(id_clientes) REFERENCES Clientes (id_clientes)
